d83bf67039d2e7bfc4ec6b5d2609bca278d36824
Javier Salgado <javier.andres.salgado@gmail.com>
2021-08-17 11:31:11 -0400

    Update holamundo.rb

--- primeros pasos/holamundo.rb
+++ primeros pasos/holamundo.rb
@@ -5,3 +5,13 @@ titulo3 = "CHILE"
 puts titulo1
 puts titulo2
 puts titulo3
+
+
+var_texto = "tenemos variables que pueden ser texto o numericas"
+var_num = 1
+var_dec = 12.3
+var_boolean = false
+
+# concatenar variables
+
+texto = "#{var_texto} #{var_num} , o #{var_dec}"
