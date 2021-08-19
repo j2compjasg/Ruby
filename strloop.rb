7d55c377964b73a596974b2dd621e451f9581485
Javier Salgado <javier.andres.salgado@gmail.com>
2021-08-19 18:05:52 -0400

    Update strloop.rb

--- strloop.rb
+++ strloop.rb
@@ -1,10 +1,24 @@
 
 str =""
+
+puts "Ingrese el limite del ciclo"
 str = gets.chomp
 
 limite = str.to_i
 
-for i = 0, i <= limite
-    if i%2 = 0 puts i
+for i in (1..limite)
+    if i%2 == 0 then 
+        puts i
+    end
 end
 
+puts "ingresa un numero"
+str = gets.chomp
+num1 = str.to_i
+puts "ingresa otro numero"
+str = gets.chomp
+num2 = str.to_i
+
+for i in (num1..num2)
+    puts i
+end
