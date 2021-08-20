482bb753a71327926b4423ad88a604b9b023d081
j2compjasg <86436308+j2compjasg@users.noreply.github.com>
2021-08-19 20:52:50 -0400

    clase de 19-08

--- arreglos.rb
+++ arreglos.rb
@@ -8,3 +8,22 @@ puts titulo3
 
 pajaros = ["Aguila","Bandurria","Chincol","Condor"]
 
+puts "Imprime el Array completo"
+puts pajaros
+
+puts "Impreim Bandurria, elemento 1 del Array"
+puts pajaros[1]
+
+puts "Imprime Condor, elemento -1 del array"
+puts pajaros [-1]
+
+puts "imprime cantidad de elementos del array"
+puts pajaros.length
+
+puts "Imprime el primer elemento y el ultimo de un array"
+
+puts pajaros.first
+puts pajaros.last
+
+puts "Rango:  Imprime los valores entre el 2 y 3.  Chincol y Condor"
+puts pajaros [2..3]
