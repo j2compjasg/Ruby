7e1acd44b74b612dd00655777c123da2d173c8b3
j2compjasg <86436308+j2compjasg@users.noreply.github.com>
2021-08-23 22:23:33 -0400

    aa

--- api.rb
+++ api.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'rest-client'
 require 'json'
 require 'pp'
+require 'docx'
 
 titulo1 = "Curso de Emprendimiento Digital"
 titulo2 = "2021"
@@ -17,17 +18,29 @@ response = RestClient.get(url)
 
 cuerpo = JSON.parse(response)
 
+str = ""
 #puts cuerpo.length
 
-pp cuerpo
+#pp cuerpo
 #valor = cuerpo
 #puts cuerpo
 
-puts ""
-puts "VAMOS A RECORRER EL HASH"
-puts ""
-cuerpo.each {|key,valor| puts "Clave :#{key} , Valor: #{valor}" }
+str += "\n"
+str += "INDICADORES ECONOMICOS" + "\n"
+str += "" + "\n"
+str += "DIA " + cuerpo["fecha"].slice(0..9) + "\n"
+str += "--------------------------------" + "\n"
 
+cuerpo.each do |key,valor|
+    if key != "version" && key != "autor" && key != "fecha"
+        str += cuerpo[key]["nombre"] + " : " + cuerpo[key]["valor"].to_s + "\n"
+    end
+end
 
+puts str
 
+doc = Docx::Document.new('text.docx')
 
+doc.insert_text (str)
+
+doc.save('Nuevo.docx')
--- docx.rb
+++ docx.rb
@@ -9,6 +9,10 @@ puts titulo3
 require 'docx'
 
 doc = Docx::Document.open('prueba.docx')
-doc.paragraphs.each do |lectura| 
-    puts lectura
-end
+#doc.paragraphs.each do |lectura| 
+#    puts lectura
+#end
+
+doc.bookmarks.each_pair do |bookmark_name, bookmark_object|
+    puts bookmark_name
+  end
