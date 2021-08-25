cf333583bcb34e56a082ef04ffd3a429fb0c39a7
j2compjasg <86436308+j2compjasg@users.noreply.github.com>
2021-08-24 20:07:07 -0400

    aa

--- api.rb
+++ api.rb
@@ -4,6 +4,33 @@ require 'json'
 require 'pp'
 require 'docx'
 
+
+def format_number(number,dec)
+    precision = 0
+    if dec > 0 
+        precision = dec
+    end
+
+    if dec > 0 then 
+        number = number.round(precision)
+    end
+    todo, decimal = number.to_s.split(".")
+    strdec = decimal.to_s
+
+    for i in(strdec.length..dec-1) 
+        strdec += "0"
+    end
+
+    num_groups = todo.chars.to_a.reverse.each_slice(3)
+    todo_y_coma = num_groups.map(&:join).join(',').reverse
+
+    if precision > 0 then 
+        return [todo_y_coma, strdec].compact.join(".")
+    else
+        return todo_y_coma
+    end
+end
+
 titulo1 = "Curso de Emprendimiento Digital"
 titulo2 = "2021"
 titulo3 = "CHILE"
@@ -14,12 +41,13 @@ puts titulo3
 
 url = 'https://mindicador.cl/api'
 
+
+
+puts 'Presione ENTER para conectar A API '
+puts '-----------------------------------'
+a = gets.chomp
+
 response = RestClient.get(url)
-
-puts 'CONECTANDO A API '
-puts '-----------------'
-
-
 cuerpo = JSON.parse(response)
 
 puts 'API descargada completamente'
@@ -41,12 +69,14 @@ puts
 
 #doc.bookmarks['fecha'].insert_after(str)
 
+
+puts '-----------------------------------------------------'
 cuerpo.each do |key,valor|
     if key != "version" && key != "autor" && key != "fecha"
-        puts cuerpo[key]["nombre"] + " : " + cuerpo[key]["valor"].to_s
+        puts cuerpo[key]["nombre"].ljust(40) + " : " + format_number(cuerpo[key]["valor"],2).to_s.rjust(10)
     end
 end
-
+puts '-----------------------------------------------------'
 puts 'FIN Indicadores'
 
 
---
+++ fromnum.rb
@@ -0,0 +1,36 @@
+
+def format_number(number,dec)
+    precision = 0
+    if dec > 0 
+        precision = dec
+    end
+    puts "NUMERO INICIAL #{number}"
+    if dec > 0 then 
+        number = number.round(precision)
+        puts "truncado de numero en #{number}"
+    end
+    todo, decimal = number.to_s.split(".")
+    strdec = decimal.to_s
+    puts "ENTERO = #{todo} DECIMAL = #{strdec} PRECISION = #{dec}"
+    for i in(strdec.length..dec-1) 
+        strdec += "0"
+    end
+    num_groups = todo.chars.to_a.reverse.each_slice(3)
+    todo_y_coma = num_groups.map(&:join).join(',').reverse
+    if precision > 0 then 
+        return [todo_y_coma, strdec].compact.join(".")
+    else
+        return todo_y_coma
+    
+    end
+end
+
+
+#numero = 246574.256654
+numero = 32432
+#strnum = numero.round(2)
+#puts "el numero es " + strnum.to_s + "- "
+
+puts format_number(numero,2)
+
+
